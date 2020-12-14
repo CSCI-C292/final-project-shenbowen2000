c3e5351aefd77ae22d4af70630bbdc85bb2d058b
Bowen Shen <bowshen@iu.edu>
2020-12-13 21:58:35 -0500

    Final project

---
+++ Assets/Script/FoodMaker.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodMaker : MonoBehaviour
+{
+    private static FoodMaker _instance;
+    public static FoodMaker Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public float xlimit = 0;
+    public float ylimit = 0;
+    public GameObject foodPrefab;
+    public GameObject rewardPrefab;
+    public Sprite[] foodSprites;
+    private Transform foodHolder;
+
+    void Awake()
+    {
+        _instance = this;
+        ylimit = Camera.main.orthographicSize;
+        xlimit = ylimit * Screen.width / Screen.height;
+    }
+
+    void Start()
+    {
+        foodHolder = GameObject.Find("FoodHolder").transform;
+        MakeFood(false);
+    }
+
+    public void MakeFood(bool isReward)
+    {
+        int index = Random.Range(0, foodSprites.Length);
+        GameObject food = Instantiate(foodPrefab);
+        food.GetComponent<Image>().sprite = foodSprites[index];
+        Debug.Log(foodSprites.Length);
+        Debug.Log(index);
+        food.transform.SetParent(foodHolder, false);
+        float x = Random.Range(0, xlimit);
+        float y = Random.Range(0, ylimit);
+        food.transform.localPosition = new Vector3(x * 30, y * 30, 0);
+        if (isReward)
+        {
+            GameObject reward = Instantiate(rewardPrefab);
+            reward.transform.SetParent(foodHolder, false);
+            x = Random.Range(0, xlimit);
+            y = Random.Range(0, ylimit);
+            reward.transform.localPosition = new Vector3(x * 30, y * 30, 0);
+        }
+    }
+}
